f29e2f2c559f2977116d16b423a3dda7012f933f
miguelperalta <miguel.aepl88@gmail.com>
2022-04-21 03:13:22 -0400

    Reemplazar Ñ por N

---
+++ script.js
@@ -0,0 +1,33 @@
+//REEMPLAZAR CARACTER Ñ POR N
+let palabra
+let nuevaPalabra
+let marca=0
+do {
+    palabra=prompt("Ingrese una palabra o frase con ñ").toLowerCase()
+    for (let i = 0; i< palabra.length; i++) {
+        let caracter = palabra.charAt(i)
+        if(i==0){
+            if(caracter == "ñ") {
+                marca=1
+                nuevaPalabra="n"
+                
+             }else{
+                nuevaPalabra=caracter
+             }
+        }else{
+            if(caracter == "ñ") {
+                marca=1
+                nuevaPalabra=nuevaPalabra+"n"
+                
+             }else{
+                nuevaPalabra=nuevaPalabra+caracter
+             }
+        }
+   }
+   if(marca==0){
+       alert("Su palabra o frase no contiene Ñ")
+   }else{
+    alert("La nueva palabra o frase es: " + nuevaPalabra)
+   }
+} while (marca==0)
+
